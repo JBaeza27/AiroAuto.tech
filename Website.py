1015133a808529f4335ea4f56af97df4165b6550
Yousef Ali <yousefali0017@gmail.com>
2023-10-07 16:43:48 -0500

    Update Website.py

--- Website.py
+++ Website.py
@@ -1,1 +1,9 @@
 #Streamlit
+import streamlit as st
+
+
+st.set_page_config(page_title="Airo", page_icon=":tada:", layout="wide")
+
+st.subheader("Hi, this is Airo tech :wave:");
+st.title("A Data Analyst From Germany")
+st.write("i am passabout aidieiei")
