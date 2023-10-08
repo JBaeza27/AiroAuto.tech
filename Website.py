e7a44179d029b0d7b0865b2ddb094199ebb78fd8
Ashfaque Rahman-Danish <rizvy204@gmail.com>
2023-10-07 21:25:43 -0500

    Update Website.py

--- Website.py
+++ Website.py
@@ -5,6 +5,5 @@ import streamlit as st
 st.set_page_config(page_title="Airo Auto", page_icon=":tada:", layout="wide")
 
 st.subheader("Airo Auto");
-st.subheader("Comparison of Automobile Aeurodynamics")
-st.title("A Data Analyst From Germany")
-st.write("i am passabout aidieiei")
+st.title("Comparison of Automobile Aerodynamics")
+st.write("lol")
