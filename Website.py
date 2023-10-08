90ff0eed4b5e8401887d40ac1b719075fe523b44
Ashfaque Rahman-Danish <rizvy204@gmail.com>
2023-10-07 21:50:25 -0500

    Update Website.py

--- Website.py
+++ Website.py
@@ -7,19 +7,19 @@ st.set_page_config(page_title="Airo Auto", page_icon=":tada:", layout="wide")
 st.subheader("Airo Auto");
 st.title("Comparison of Automobile Aerodynamics")
 Make = st.selectbox(
-...     'Select the make of the vehicle',
-...     ('Email', 'Home phone', 'Mobile phone'))
->>>
->>> st.write('You selected:', Make)
+     'Select the make of the vehicle',
+    ('Email', 'Home phone', 'Mobile phone'))
+
+st.write('You selected:', Make)
 
 Model = st.selectbox(
-...     'Select the model of the vehicle',
-...     ('Email', 'Home phone', 'Mobile phone'))
->>>
->>> st.write('You selected:', Model)
+    'Select the model of the vehicle',
+    ('Email', 'Home phone', 'Mobile phone'))
+
+st.write('You selected:', Model)
 
 Year = st.selectbox(
-...     'Select the year of the vehicle',
-...     ('Email', 'Home phone', 'Mobile phone'))
->>>
->>> st.write('You selected:', Year)
+    'Select the year of the vehicle',
+    ('Email', 'Home phone', 'Mobile phone'))
+
+st.write('You selected:', Year)
