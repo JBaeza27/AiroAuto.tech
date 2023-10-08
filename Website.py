78859229ffd5e9dcd0c8d506ece8ef51594a1782
Yousef Ali <yousefali0017@gmail.com>
2023-10-07 23:14:58 -0500

    Update Website.py

--- Website.py
+++ Website.py
@@ -6,25 +6,48 @@ st.set_page_config(page_title="Airo Auto", page_icon=":tada:", layout="wide")
 
 st.markdown('<h2 style="font-size: 40px;">Airo Auto</h2>', unsafe_allow_html=True)
 st.title("Comparison of Automobile Aerodynamics")
-<<<<<<< HEAD
 st.write("lol")
->>>>>>> e7a44179d029b0d7b0865b2ddb094199ebb78fd8
-=======
-Make = st.selectbox(
+
+col1, col2 = st.columns(2)
+
+with col1:
+    st.markdown('<h2 style="font-size: 30px;">Vehicle 1</h2>', unsafe_allow_html=True)
+
+    Make = st.selectbox(
      'Select the make of the vehicle',
     ('Email', 'Home phone', 'Mobile phone'))
 
-st.write('You selected:', Make)
+    st.write('You selected:', Make)
 
-Model = st.selectbox(
-    'Select the model of the vehicle',
+    Model = st.selectbox(
+        'Select the model of the vehicle',
+        ('Email', 'Home phone', 'Mobile phone'))
+
+    st.write('You selected:', Model)
+
+    Year = st.selectbox(
+        'Select the year of the vehicle',
+        ('Email', 'Home phone', 'Mobile phone'))
+
+    st.write('You selected:', Year)
+
+with col2:
+    st.markdown('<h2 style="font-size: 30px;">Vehicle 2</h2>', unsafe_allow_html=True)
+
+    Make2 = st.selectbox(
+     'Select the make of the vehicle ',
     ('Email', 'Home phone', 'Mobile phone'))
 
-st.write('You selected:', Model)
+    st.write('You selected:', Make2)
 
-Year = st.selectbox(
-    'Select the year of the vehicle',
-    ('Email', 'Home phone', 'Mobile phone'))
+    Model2 = st.selectbox(
+        'Select the model of the vehicle ',
+        ('Email', 'Home phone', 'Mobile phone'))
 
-st.write('You selected:', Year)
->>>>>>> e91e3e8975658854f5b2906d884d9606a5f434d5
+    st.write('You selected:', Model2)
+
+    Year2 = st.selectbox(
+        'Select the year of the vehicle ',
+        ('Email', 'Home phone', 'Mobile phone'))
+
+    st.write('You selected:', Year2)
