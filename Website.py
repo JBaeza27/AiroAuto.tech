7212fe4fae535d654aca13e038aa7abd8d4b1bc4
Yousef Ali <yousefali0017@gmail.com>
2023-10-07 21:33:32 -0500

    Update Website.py

--- Website.py
+++ Website.py
@@ -5,6 +5,6 @@ import streamlit as st
 st.set_page_config(page_title="Airo Auto", page_icon=":tada:", layout="wide")
 
 st.subheader("Airo Auto");
-st.subheader("Comparison of Automobile Aeurodynamics")
-st.title("A Data Analyst From Germany")
+st.title("Comparison of Automobile Aeurodynamics")
+st.title("Comparison")
 st.write("i am passabout aidieiei")
